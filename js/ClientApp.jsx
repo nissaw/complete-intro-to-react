ce38289b7de0939d07fa0a2160f3cafbdf32ffd0
Nissa Wollum <nissawollum@gmail.com>
2016-04-05 22:39:12 -0700

    Up to fem-11

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,19 +2,15 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
-const { Router, Route, hashHistory } = require('react-router')
-
-// the following 4 lines are summarized by line4 'destructuring'
-// const ReactRouter = require('react-router')
-// const Router = require('router');
-// const Route = require('route');
-// const hashHistory = require('hashHistory');
+const Layout = require('./Layout')
+const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 
 const App = () => (
-  // this is the easier way to keeo track of back/forward button Use - will be replaced by browser histroty when we get to the node side of things
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 
---
+++ react_notes.md
@@ -0,0 +1,22 @@
+props (data) down => actions up
+one way data binding
+
+
+const { Router, Route, hashHistory } = require('react-router')
+
+// the following 4 lines are summarized by line4 'destructuring'
+// const ReactRouter = require('react-router')
+// const Router = require('router');
+// const Route = require('route');
+// const hashHistory = require('hashHistory');
+
+JSON.stringify(the data you want to display, a replacer function, indentations)
+
+const { string } = React.PropTypes // with destructuring
+const string = React.PropTypes.string // without destructuring
+
+hashHistory is =the easier way to keep track of back/forward button Use - will be replaced by browser history when we get to the node side of things
+
+One index route per nesting
+
+  {/* this is how you do comments in jsx */}
