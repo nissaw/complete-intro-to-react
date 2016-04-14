1a5bfbe29f05c92c070e1490ba439a0fdcabe03f
Nissa Wollum <nissawollum@gmail.com>
2016-04-13 21:49:03 -0700

    Starts Universal Rendering, BrowserEntry created

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,5 +1,4 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
@@ -11,22 +10,17 @@ const { Provider } = require('react-redux') // anytime you use the connector is 
 const App = React.createClass({
   render () {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Layout}>
-          <IndexRoute component={Landing} />
-          <Route path='/search' component={Search} />
-          <Route path='/details/:id' component={Details} />
-        </Route>
-      </Router>
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path='/' component={Layout}>
+            <IndexRoute component={Landing} />
+            <Route path='/search' component={Search} />
+            <Route path='/details/:id' component={Details} />
+          </Route>
+        </Router>
+      </Provider>
     )
   }
 })
 
-ReactDOM.render(
-(
-  <Provider store={store}>
-    <App />
-  </Provider>
-),
-document.getElementById('app')
-)
+module.exports = App
